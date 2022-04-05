247d6d34811a2856859623c301ade2e3f41cab8c
HAMA2 <Haitham.magdy@mondia.com>
2022-04-06 00:52:44 +0200

    add changes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:4000/api/v0",
+  apiHost: "https://Udagram-env.eba-66qw2cpz.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
