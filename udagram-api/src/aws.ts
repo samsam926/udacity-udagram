3be8b166e7e39ffecbf7d4ed1ad40027c0443d33
HAMA2 <Haitham.magdy@mondia.com>
2022-04-07 14:09:33 +0200

    add changes

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -12,6 +12,7 @@ export const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN
   },
 });
 
--- udagram-frontend/deploy/deploy.sh
+++ udagram-frontend/deploy/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-965910973425/
+aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend113/
