6d0f50bbfce82fb41ec734e9eb972b34e83d70f2
HAMA2 <Haitham.magdy@mondia.com>
2022-04-12 22:55:17 +0200

    add changes

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,16 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(
-  `postgres://${config.username}:${config.password}@${config.host}:${config.dbPort}/${config.database}`
-);
-// export const sequelize = new Sequelize({
-//   username: config.username,
-//   password: config.localPassword,
-//   database: config.database,
-//   host: config.localhost,
-//   port: config.dbPort,
-
-//   dialect: "postgres",
-//   storage: ":memory:",
-// });
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.dbPort,
+  dialect: "postgres",
+  storage: ":memory:",
+});
